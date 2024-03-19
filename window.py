2603a0c8ecaec8f90aa62a681dc1eec44f766f9d
Alice Benatti <alice17bee@gmail.com>
2024-03-19 09:12:31 +0100

    optimized config dark mode

--- window.py
+++ window.py
@@ -47,15 +47,10 @@ img = ImageTk.PhotoImage(img)
 # Create a text to display
 text = tk.Label(window, text="Today recommended read, to improve your Informatics skill, is:",
                 font=("Comic Sans MS", 40, "bold"), fg='white', bg='#02111B')
-# Set the window background to dark csunibo color
-text.config(background='#02111B')
 text.pack()
 
 # Create a label to display the image
-label = tk.Label(window, image=img)
-# Set the window background to dark csunibo color
-label.config(background='#02111B')
-
+label = tk.Label(window, image=img, background='#02111B')
 label.pack(fill=tk.BOTH, expand=tk.YES)
 
 # Bind escape key to exit fullscreen
